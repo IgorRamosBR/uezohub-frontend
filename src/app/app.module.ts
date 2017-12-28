6733fc0d8a14eabd08bf766730ecfd73a58f11d9
IgorRamosBR <igorrpestana@yahoo.com.br>
2017-12-28 00:06:50 -0200

    index component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
