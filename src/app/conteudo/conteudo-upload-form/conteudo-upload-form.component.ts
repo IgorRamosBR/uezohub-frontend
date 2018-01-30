f362e7658a6be0759855c4f7b2db6fcd7925b507
= <=>
2018-01-30 17:33:17 -0200

    input file upload

--- src/app/conteudo/conteudo-upload-form/conteudo-upload-form.component.ts
+++ src/app/conteudo/conteudo-upload-form/conteudo-upload-form.component.ts
@@ -1,4 +1,9 @@
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { ConteudoService } from './../conteudo.service';
+import { DisciplinaService } from './../../disciplina/disciplina.service';
 import { Component, OnInit } from '@angular/core';
+import { CursoService } from '../../cursos/curso.service';
+import { Conteudo } from '../conteudo';
 
 @Component({
   selector: 'app-conteudo-upload-form',
@@ -7,16 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConteudoUploadFormComponent implements OnInit {
 
-  panelOpenState: boolean = false;
-  foods = [
-    {value: 'steak-0', viewValue: 'Ciência da Computação'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
+  nomeDoArquivo = '';
+  conteudo = new Conteudo();
+  cursos = [];
+  disciplinas = [];
 
-  constructor() { }
+  constructor(
+    private cursoService: CursoService,
+    private disciplnaService: DisciplinaService,
+    private conteudoService: ConteudoService,
+    private errorHandlerService: ErrorHandlerService
+  ) { }
 
   ngOnInit() {
+    this.carregaCursos();
+    this.carregaDisciplinas();
   }
 
+  upload(event){ 
+    if( event.target.files && event.target.files.length > 1 )
+				this.nomeDoArquivo = ( event.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', event.target.files.length );
+			else if( event.target.value )
+		  	this.nomeDoArquivo = event.target.value.split( '\\' ).pop();
+  } 
+
+  carregaCursos() {
+    return this.cursoService.buscarTodos()
+      .then(cursos => {
+        this.cursos = cursos.map( c => {
+          return { value: c.id, viewValue: c.nome };
+        });
+      });
+  }
+
+  carregaDisciplinas() {
+    return this.disciplnaService.buscarTodos()
+      .then(disciplinas => {
+        this.disciplinas = disciplinas.map( d => {
+          return { value: d.id, viewValue: d.nome };
+        });
+      });
+  }
 }
--- src/app/conteudo/conteudo.ts
+++ src/app/conteudo/conteudo.ts
@@ -1,7 +1,14 @@
+import { Disciplina } from "../disciplina/disciplina";
+
 export class Conteudo {
     id: number;
     nome: string;
     descricao: string;
     extensao: string;
     link: string;
+    disciplina: Disciplina;
+
+    constructor () {
+        this.disciplina = new Disciplina();
+    }
 }
