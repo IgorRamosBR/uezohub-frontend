475204b9bb0f0802c8e43e7218bb9b04dd028f98
IgorRamosBR <igorir7@gmail.com>
2018-06-11 19:37:30 -0300

    minor to deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://34.205.71.183:8080'
+  API_BASE_URL: 'http://uezohub-classic-lb-1950583630.us-east-1.elb.amazonaws.com'
 };
